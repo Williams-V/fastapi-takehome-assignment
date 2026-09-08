5d2a52af7078267cf05bdb30a23fc89911c2dc2f
williams-v <vanessaadhiambo04@gmail.com>
2026-09-08 11:45:12 +0300

    Add CORS support for frontend

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI, Header, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, Field
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3001", "http://127.0.0.1:3001"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 API_KEY = "cloudwise123"
 
